ec345fdc9fd65193a8476c62e253f81f486d14a9
hhhhhli <1401784262@qq.com>
2025-11-25 11:21:56 +0800

    add rviz config

--- launch/viz.launch.py
+++ launch/viz.launch.py
@@ -10,6 +10,20 @@ import os
 
 def generate_launch_description():
 
+    rviz_config_file = PathJoinSubstitution([
+        FindPackageShare('benchmarking_3dcpp'),
+        'rviz',
+        'benchmarking_3dcpp.rviz'
+    ])
+
+    rviz2_node = Node(
+        package='rviz2',
+        executable='rviz2',
+        name='rviz2', 
+        output='screen',
+        arguments=['-d', rviz_config_file]
+    )
+
     benchmarking_3dcpp_node = Node(
         package='benchmarking_3dcpp',
         executable='benchmarking_viz_node',
@@ -19,6 +33,7 @@ def generate_launch_description():
     )
 
     return LaunchDescription([
+        rviz2_node,
         benchmarking_3dcpp_node
     ])
 
